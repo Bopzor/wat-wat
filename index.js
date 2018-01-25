a74d0103101398baaa730a9d59bb4f9c083e2ae9
Bopzor <violaine.ballandin@gmail.com>
2018-01-25 20:47:53 +0100

    layout of movie content

--- index.js
+++ index.js
@@ -2,36 +2,37 @@ const BASE_URL = 'http://localhost:4269';
 const BASE_CONTENT_URL = 'http://www.omdbapi.com/?apikey=8ce98bc8&t='
 
 const MOVIE_TEMPLATE = '\
-<li class="list-item" id="movie-MOVIE_ID">\
+<li class="list-item" id="movie-MOVIE_ID" draggable="true" ondrag="sortMovie()">\
     <div class="remove-button">\
         <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-button--colored" style="width: 20px; height: 20px; min-width: initial;" onclick="deleteMovie(MOVIE_ID)">\
             <i class="material-icons md-18">remove</i>\
         </button>\
     </div>\
+    </div>\
     <div class="movie-title" onclick="enlarge(MOVIE_ID)">\
         MOVIE_TITLE\
     </div>\
     <div class="movie-content">\
-        <img class="movie-poster" src="MOVIE_POSTER">\
-        <div class="movie-released">MOVIE_RELEASED</div>\
-        <div class="movie-runtime">MOVIE_RUNTIME</div>\
-        <div class="movie-plot">MOVIE_PLOT</div>\
-        <div class="movie_director">MOVIE_DIRECTOR</div>\
-        <div class="movie-writer">MOVIE_WRITER</div>\
-        <div class="movie-actors">MOVIE_ACTORS</div>\
+        <img class="movie-poster" src="MOVIE_POSTER" alt="MOVIE_TITLE">\
+        <div class="movie-released"><div class="content">Released:</div> MOVIE_RELEASED</div>\
+        <div class="movie-runtime"><div class="content">Runtime:</div> MOVIE_RUNTIME</div>\
+        <div class="movie-plot"><div class="content">Plot:</div> MOVIE_PLOT</div>\
+        <div class="movie_director"><div class="content">Director:</div> MOVIE_DIRECTOR</div>\
+        <div class="movie-writer"><div class="content">Writer:</div> MOVIE_WRITER</div>\
+        <div class="movie-actors"><div class="content">Actors:</div> MOVIE_ACTORS</div>\
     </div>\
 </li>';
 
 function createMovie(movie) {
     let template = MOVIE_TEMPLATE;
 
-    template= template.replace(/MOVIE_POSTER/g, movie.Poster);
-    template = template.replace(/MOVIE_RELEASED/g, movie.Released);
-    template = template.replace(/MOVIE_RUNTIME/g, movie.Runtime);
-    template = template.replace(/MOVIE_Plot/g, movie.Plot);
-    template = template.replace(/MOVIE_DIRECTOR/g, movie.Director);
-    template = template.replace(/MOVIE_WRITER/g, movie.Writer);
-    template = template.replace(/MOVIE_ACTORS/g, movie.Actors);
+    template= template.replace(/MOVIE_POSTER/g, movie.poster);
+    template = template.replace(/MOVIE_RELEASED/g, movie.released);
+    template = template.replace(/MOVIE_RUNTIME/g, movie.runtime);
+    template = template.replace(/MOVIE_PLOT/g, movie.plot);
+    template = template.replace(/MOVIE_DIRECTOR/g, movie.director);
+    template = template.replace(/MOVIE_WRITER/g, movie.writer);
+    template = template.replace(/MOVIE_ACTORS/g, movie.actors);
     template = template.replace(/MOVIE_ID/g, movie.id);
     template = template.replace(/MOVIE_TITLE/g, movie.title);
 
@@ -39,7 +40,7 @@ function createMovie(movie) {
 }
 
 function enlarge(id) {
-    $("#movie-" + id + " .movie-plot").slideToggle("slow");
+    $("#movie-" + id + " .movie-content").slideToggle("slow");
 }
 
 function addMovie() {
@@ -52,18 +53,19 @@ function addMovie() {
                 method: 'POST',
                 body: JSON.stringify({
                     title,
-                    Poster: content.Poster,
-                    Released: content.Released,
-                    Runtime: content.Runtime,
-                    Plot: content.Plot,
-                    Director: content.Director,
-                    Writer: content.Writer,
-                    Actors: content.Actors,
+                    plot: content.Plot,
+                    released: content.Released,
+                    runtime: content.Runtime,
+                    director: content.Director,
+                    writer: content.Writer,
+                    actors: content.Actors,
+                    poster: content.Poster,
+
                 }),
                 headers: new Headers({
                     'Content-Type': 'application/json',
-            })
-        };
+                })
+            };
 
             return fetch(BASE_URL + '/api/movie', opts)
                 .then(res => res.json())
@@ -95,3 +97,23 @@ function deleteMovie(id) {
         .catch(error => console.error('Error:', error))
         .then(() => $('#movie-' + id).remove());
 }
+
+
+/*function sortMovie() {
+    const opts = {
+        method: 'POST'
+        body: JSON.stringify(
+            //[new movies ids order]
+        ),
+        headers: new Headers({
+            'Content-Type': 'application/json',
+        })
+    };
+
+    return fetch(BASE_URL + '/api/movies/sort', opts)
+        .then(res => res.json())
+        .catch(error => console.error('Error:', error))
+        .then(movie => {
+            console.log('Success:', movie);
+}*/
+
