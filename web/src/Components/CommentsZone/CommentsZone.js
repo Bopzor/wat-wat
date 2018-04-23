0886d5fa587dc505b93ad9561d79e64e27e71d24
Bopzor <violaine.ballandin@gmail.com>
2018-04-23 10:38:01 +0200

    start implementing edit comment

--- web/src/Components/CommentsZone/CommentsZone.js
+++ web/src/Components/CommentsZone/CommentsZone.js
@@ -1,10 +1,54 @@
 import React, { Component } from 'react';
 import CommentInput from '../CommentInput/CommentInput.js'
-import { RemoveIconButton, EditIconButton } from '../IconsButton/IconsButton.js';
+import {
+  RemoveIconButton,
+  EditIconButton,
+  CancelEditIconButton,
+  SubmitEditIconButton,
+} from '../IconsButton/IconsButton.js';
 import './CommentsZone.css'
 
 class CommentsZone extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editCommentId: null,
+      newComment: '',
+    }
+  }
+
+  onClick(id) {
+      this.setState({ editCommentId: id })
+    };
+
   createMovieComment(comment) {
+
+
+    if(this.state.editCommentId === comment.id) {
+      return (
+          <div key={comment.id} className='comment'>
+
+            <div className='comment-text'>
+
+              <div className="comment-author">{comment.author} :</div>
+              <input
+                rows="2"
+                defaultValue={comment.comment}
+                onChange={e => this.setState({ newComment: e.target.value })}
+              />
+            </div>
+            <div className='comment-buttons'>
+
+              <SubmitEditIconButton onClick={() => this.props.submitUpdateComment(this.state.newComment)}/>
+              <CancelEditIconButton />
+
+            </div>
+
+          </div>
+      )
+    }
+
     return (
       <div key={comment.id} className='comment'>
 
@@ -17,7 +61,7 @@ class CommentsZone extends Component {
 
         <div className='comment-buttons'>
 
-          <EditIconButton />
+          <EditIconButton onClick={() => this.onClick(comment.id)}/>
           <RemoveIconButton onClick={() => this.props.removeMovieComment(comment)} />
 
         </div>
