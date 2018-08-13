83ce3ab9ddc4936f4abbd8df69883530af7399fd
Bopzor <violaine.ballandin@gmail.com>
2018-08-13 21:53:50 +0200

    add debug log on seeders

--- seeders/20180611205140-movies.js
+++ seeders/20180611205140-movies.js
@@ -4,10 +4,12 @@ const movies = require('./db.movies.json');
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-      return queryInterface.bulkInsert('movies', movies, {});
+      return queryInterface.bulkInsert('movies', movies, {})
+        .catch(console.log);
   },
 
   down: (queryInterface, Sequelize) => {
-      return queryInterface.bulkDelete('movies', null, {});
+      return queryInterface.bulkDelete('movies', null, {})
+        .catch(console.log);
   },
 };
