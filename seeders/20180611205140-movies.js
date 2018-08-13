09169c7d533449b54607eefc5f9178e800a988ea
Bopzor <violaine.ballandin@gmail.com>
2018-08-13 23:03:37 +0200

    log number of movies added

--- seeders/20180611205140-movies.js
+++ seeders/20180611205140-movies.js
@@ -4,6 +4,7 @@ const movies = require('./db.movies.json');
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
+      console.log(`inserting ${movies.length} movies`);
       return queryInterface.bulkInsert('movies', movies, {})
         .catch(console.log);
   },
