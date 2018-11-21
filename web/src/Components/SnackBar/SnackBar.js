e019d1b3cd4e1a3a24e43b7fd9fea27cff936c0e
Bopzor <violaine.ballandin@gmail.com>
2018-11-21 13:21:40 +0100

    add movie or close

--- web/src/Components/SnackBar/SnackBar.js
+++ web/src/Components/SnackBar/SnackBar.js
@@ -1,6 +1,33 @@
 import React from 'react';
 import Snackbar from '@material-ui/core/Snackbar';
+import Button from '@material-ui/core/Button';
 import { GenericButton } from '../IconsButton/IconsButton.js';
+
+const renderAction = (props) => {
+  if (props.title) {
+
+    return (
+      <Button
+        onClick={() => props.onAdd(props.title)}
+        variant="outlined"
+        color="primary"
+      >
+        Add it.
+      </Button>
+    )
+  } else {
+
+    return (
+      <GenericButton
+        className="snackbar-close-button"
+        onClick={props.onClose}
+        style={{ fontSize: 24 }}
+        color="secondary"
+        icon="close"
+      />
+    )
+  }
+}
 
 const SnackBar = props => (
   <Snackbar
@@ -9,20 +36,13 @@ const SnackBar = props => (
       horizontal: 'left',
     }}
     open={props.open}
+    autoHideDuration={5000}
     onClose={props.onClose}
     ContentProps={{
       'aria-describedby': 'message-id',
     }}
     message={<span id="message-id">{props.message}</span>}
-    action={
-      <GenericButton
-        className="snackbar-close-button"
-        onClick={props.onClose}
-        style={{ fontSize: 24 }}
-        color="secondary"
-        icon="close"
-      />
-    }
+    action={renderAction(props)}
   />
 );
 
