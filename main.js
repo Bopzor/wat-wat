8296cd389e09d2497f8e56ea9883e8b249432a20
Bopzor <violaine.ballandin@gmail.com>
2019-04-17 21:39:41 +0200

    use env variable for PORT

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ const app = express();
 
 const api = require('./api/index');
 
+const PORT = process.env.PORT || 4269;
+
 const cors = require('cors');
 app.use(cors());
 
 app.use('/api', api);
 
-app.listen(4269, () => console.log('API listening on port 4269'));
+app.listen(PORT, () => console.log('API listening on port', PORT));
